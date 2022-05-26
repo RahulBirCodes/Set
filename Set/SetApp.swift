05e995b053cecbe6523ce4a7d1de71a5e3bd250e
Rahul Bir <rahulbircodez@gmail.com>
2022-05-26 15:01:42 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Rahul Bir on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
